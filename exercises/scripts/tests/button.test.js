5b56122fa32fb0e75656616105fbb0a6de8003b8
Alexandra Holstensson <ro24nz8c@students.codeinstitute.net>
2024-12-06 11:21:09 +0000

    Add files for Simons memory game

---
+++ exercises/scripts/tests/button.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const buttonClick = require("../button.js");
+
+beforeEach( () => {
+    //First exercise - DOM, uncomment line below
+    //document.body.innerHTML = "<p id='par'></p>";
+    //Second exercice - DOM
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
+
+describe("DOM tests", () => {
+    test("expects p content to change", () => {
+        buttonClick();
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
+    })
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    })
+})
